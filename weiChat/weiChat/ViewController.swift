6ef74c9d29208a1493437f5a5c028d6da130bc47
liwei <liwei@mapbar.com>
2016-01-13 14:29:04 +0800

    initial project

---
+++ weiChat/weiChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weiChat
+//
+//  Created by liwei on 16/1/13.
+//  Copyright © 2016年 com.test.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
